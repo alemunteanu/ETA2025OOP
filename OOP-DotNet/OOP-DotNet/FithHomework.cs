158d127f9f9028581dd42987c91e822ae68e538c
Alexandra-Munteanu <alexandra.munteanu@pay4later.com>
2025-03-05 16:27:13 +0200

    added latest homework for ETA 2025 Beginning Tier

---
+++ OOP-DotNet/OOP-DotNet/FithHomework.cs
@@ -0,0 +1,49 @@
+﻿namespace OOP_DotNet
+{
+    using System;
+    using System.Collections.Generic;
+
+    class FifthHomework
+    {
+        static void Main()
+        {
+            // Step 1: Get the size of the list
+            Console.Write("Please input the size of the list: ");
+            int n = int.Parse(Console.ReadLine());
+
+            // Initialize lists and variables
+            List<int> numbers = new List<int>();
+            List<int> evenNumbers = new List<int>();
+            List<int> oddNumbers = new List<int>();
+            int sumEven = 0;
+            int sumOdd = 0;
+
+            // Step 2: Input numbers into the list
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Please input a number for List[{i}]: ");
+                int number = int.Parse(Console.ReadLine());
+                numbers.Add(number);
+
+                // Check if the number is even or odd
+                if (number % 2 == 0)
+                {
+                    evenNumbers.Add(number);
+                    sumEven += number;
+                }
+                else
+                {
+                    oddNumbers.Add(number);
+                    sumOdd += number;
+                }
+            }
+
+            // Step 3: Display the results
+            Console.WriteLine("\nYour list contains the following items: " + string.Join(", ", numbers));
+            Console.WriteLine("The list of even numbers is: " + string.Join(", ", evenNumbers));
+            Console.WriteLine("The list of odd numbers is: " + string.Join(", ", oddNumbers));
+            Console.WriteLine("\nThe sum of even numbers is: " + sumEven);
+            Console.WriteLine("The sum of odd numbers is: " + sumOdd);
+        }
+    }
+}
---
+++ OOP-DotNet/OOP-DotNet/Multimi.cs
@@ -0,0 +1,42 @@
+﻿namespace OOP_DotNet
+{
+    public class Multimi
+    {
+
+        [Test]
+        public void Test1()
+        {
+            Dictionary<string, string> objects = new Dictionary<string, string>
+        {
+            { "object", "laptop" },
+            { "fruit", "apple" },
+            { "sport", "snowboard" }
+        };
+
+            //foreach (var obj in objects)
+            //{
+            //    Console.WriteLine($"The key is {obj.Key}");
+            //    Console.WriteLine($"The value is {obj.Value}");
+            //}
+
+            Dictionary<string, List<string>> classifications = new Dictionary<string, List<string>>();
+            List<string> citiesOfRomania = new List<string> { "Bucharest", "Cluj-Napoca", "Timisoara" };
+            List<string> citiesOfFrance = new List<string> { "Paris", "Lyon", "Marseille" };
+            List<string> citiesOfGermany = new List<string> { "Berlin", "Munich", "Hamburg" };
+
+            //classifications.Add("Romania", citiesOfRomania);
+            classifications["Romania"] = citiesOfRomania;
+            classifications["France"] = citiesOfFrance;
+            classifications["Germany"] = citiesOfGermany;
+
+            foreach (var key in classifications.Keys)
+            {
+                Console.WriteLine($"The country is: {key}");
+                Console.WriteLine($"Cities are: " + string.Join(", ", classifications[key]));
+            }
+        }
+
+
+
+    }
+}
